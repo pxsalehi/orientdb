fc17440464238b10cc506ce720990ddadc30ab82
laa <lomakin.andrey@gmail.com>
2020-05-08 10:31:59 +0300

    Comment out index security decorator.

--- core/src/main/java/com/orientechnologies/orient/core/index/IndexStreamSecurityDecorator.java
+++ core/src/main/java/com/orientechnologies/orient/core/index/IndexStreamSecurityDecorator.java
@@ -7,10 +7,10 @@ import java.util.stream.Stream;
 
 public class IndexStreamSecurityDecorator {
   public static Stream<ORawPair<Object, ORID>> decorateStream(OIndex originalIndex, Stream<ORawPair<Object, ORID>> stream) {
-    return stream.filter((pair) -> OIndexInternal.securityFilterOnRead(originalIndex, pair.second) != null);
+    return stream;//stream.filter((pair) -> OIndexInternal.securityFilterOnRead(originalIndex, pair.second) != null);
   }
 
   public static Stream<ORID> decorateRidStream(OIndex originalIndex, Stream<ORID> stream) {
-    return stream.filter((rid) -> OIndexInternal.securityFilterOnRead(originalIndex, rid) != null);
+    return stream;//stream.filter((rid) -> OIndexInternal.securityFilterOnRead(originalIndex, rid) != null);
   }
 }
